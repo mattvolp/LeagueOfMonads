89537a30411d6a26e5ea779c5347cc3313b28a78
Anthony Leatherwood <aleatherwood@sharedsvs.com>
2017-08-31 15:36:27 -0400

    added union test

---
+++ src/LeagueOfMonads.Tests/UnionTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeagueOfMonads.Tests
+{
+   [TestClass]
+   public class UnionTests
+   {
+      [TestMethod]
+      public void X()
+      {
+         var u = new TestUnion(3);
+         var a = u.Map(i => i.ToString(), s => s);
+
+         Assert.AreEqual("3", a);
+      }
+
+      private class TestUnion
+         : Union<int, string>
+      {
+         public TestUnion(int item) 
+            : base(item)
+         {
+         }
+
+         public TestUnion(string item) 
+            : base(item)
+         {
+         }
+      }
+   }   
+}
