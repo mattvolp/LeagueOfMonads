57cbdccf836a01e508e5e143caef036fb69bf125
Anthony Leatherwood <adleatherwood@users.noreply.github.com>
2018-02-06 09:44:26 -0500

    v1.5.1 release
    fixed issue with Tea function not awaiting properly.

--- build-all.bat
+++ build-all.bat
@@ -1,6 +1,7 @@
 @ECHO off
 
-SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;
+REM SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;
+SET PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin
 
 msbuild.exe build-all.msbuild
 
