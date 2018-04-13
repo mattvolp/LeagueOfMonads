451db7e5747e69c389ab404e564ca7e7c24900e3
Anthony Leatherwood <adleatherwood@users.noreply.github.com>
2018-04-13 13:43:56 -0400

    wip

--- pre-build.ps1
+++ pre-build.ps1
@@ -1,6 +1,6 @@
 
-$old_version = '1.5.2.2'
-$gnu_version = '1.5.2.3'
+$old_version = '1.5.2.3'
+$gnu_version = '1.5.2.4'
 $files = ".\src\LeagueOfMonads.Core\LeagueOfMonads.Core.csproj", `
          ".\src\LeagueOfMonads.Core\Properties\AssemblyInfo.cs"
 
