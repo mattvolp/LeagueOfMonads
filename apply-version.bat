afe658b32c50bf0a6277030801f5d6f697a20faf
Anthony Leatherwood <adleatherwood@users.noreply.github.com>
2018-04-13 09:01:54 -0400

    Version update

--- apply-version.bat
+++ apply-version.bat
@@ -3,7 +3,16 @@
 SET VERSION=1.5.2.1
 SET PATH=3rd\Ssed
 
-ssed.exe "s/<Version>.*<\/Version>/<Version>%VERSION%<\/Version>/g" ".\src\LeagueOfMonads.Core\LeagueOfMonads.Core.csproj"
-ssed.exe "s/Version\(.*\)/Version\(%VERSION%\)/g" ".\src\LeagueOfMonads.Core\Properties\AssemblyInfo.cs"
+SET CSPROJ_BAK=".\src\LeagueOfMonads.Core\LeagueOfMonads.Core.csproj.bak"
+SET ASSEMB_BAK=".\src\LeagueOfMonads.Core\Properties\AssemblyInfo.cs.bak"
+
+IF EXIST %CSPROJ_BAK% DEL /F /Q %CSPROJ_BAK%
+IF EXIST %ASSEMB_BAK% DEL /F /Q %ASSEMB_BAK%
+
+ssed.exe -i.bak "s/<Version>.*<\/Version>/<Version>%VERSION%<\/Version>/g" ".\src\LeagueOfMonads.Core\LeagueOfMonads.Core.csproj"
+ssed.exe -i.bak "s/Version\(.*\)/Version\(%VERSION%\)/g" ".\src\LeagueOfMonads.Core\Properties\AssemblyInfo.cs"
+
+DEL /F /Q %CSPROJ_BAK%
+DEL /F /Q %ASSEMB_BAK%
 
 PAUSE
