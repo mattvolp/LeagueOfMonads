e6b8b4617386ed959c1c926032a5c3a2483a4c3a
Anthony Leatherwood <adleatherwood@users.noreply.github.com>
2018-06-15 16:59:26 -0400

    wip

--- zero29.sh
+++ zero29.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-tar -xvzf zero29-core.tar.gz --overwrite
+tar -xvzf zero29-core.tar.gz --overwrite &>/dev/null
 
 dotnet Zero29.Core.dll "$@"
 
-rm -f FSharp.Core.dll
-rm -f Zero29.Core.dll
-rm -f Zero29.Core.runtimeconfig.json
+rm -f FSharp.Core.dll                    &>/dev/null
+rm -f Zero29.Core.dll                    &>/dev/null
+rm -f Zero29.Core.runtimeconfig.json     &>/dev/null
