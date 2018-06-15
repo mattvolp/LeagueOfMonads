a1f12476cd5c3e82b26d91bf3e163b36336c958c
Anthony Leatherwood <adleatherwood@users.noreply.github.com>
2018-06-15 19:38:28 -0400

    removed versioning

--- zero29.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-tar -xvzf zero29-core.tar.gz --overwrite &>/dev/null
-
-dotnet Zero29.Core.dll "$@"
-
-rm -f FSharp.Core.dll                    &>/dev/null
-rm -f Zero29.Core.dll                    &>/dev/null
-rm -f Zero29.Core.runtimeconfig.json     &>/dev/null
